0ee81e277bc511532dc29be5519ad78c9177e95c
Zenky <bzenky@gmail.com>
2023-02-15 06:40:27 -0300

    feat: google fonts

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,14 +1,7 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  display: flex;
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #000;
-`
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
+  padding: 24px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 `
