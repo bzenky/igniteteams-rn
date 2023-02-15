b10b4d7a9e4711522043668da2889f19cab9376d
Zenky <bzenky@gmail.com>
2023-02-15 06:06:29 -0300

    feat: styled components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 32px;
+`
