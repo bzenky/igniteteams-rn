8ecc5433f436f521195f6c9b9db45156485ca6b9
Zenky <bzenky@gmail.com>
2023-02-15 21:48:43 -0300

    feat: header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+`
+
+export const Logo = styled.Image`
+  width: 46px;
+  height: 55px;
+`
