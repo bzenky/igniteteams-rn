70f2c89b7fb9bbcd974dba3e4d353de7445f315a
Zenky <bzenky@gmail.com>
2023-02-16 06:37:28 -0300

    feat: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
+
   return (
     <Container>
       <Header />
@@ -13,7 +17,15 @@ export function Groups() {
         subtitle='jogue com sua turma'
       />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
     </Container>
   )
 }
