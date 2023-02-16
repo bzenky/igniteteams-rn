fc27454f69c3dcd390301a668d82c111824e321a
Zenky <bzenky@gmail.com>
2023-02-15 22:10:36 -0300

    feat: phosphor icons

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,4 @@
+import { CaretLeft } from "phosphor-react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -12,3 +13,12 @@ export const Logo = styled.Image`
   width: 46px;
   height: 55px;
 `
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+  size: 32,
+  color: theme.COLORS.WHITE
+}))``
