2e3b1a6b71ec550a63d091eec8e9cfa104754cf9
Zenky <bzenky@gmail.com>
2023-02-16 06:25:53 -0300

    feat: groupcard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
@@ -6,10 +7,13 @@ export function Groups() {
   return (
     <Container>
       <Header />
+
       <Highlight
         title='Turmas'
         subtitle='jogue com sua turma'
       />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   )
 }
