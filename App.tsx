44b6deb9ab404a054ec95ef74aa76efc5ea53802
Zenky <bzenky@gmail.com>
2023-02-15 21:42:55 -0300

    style: status bar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 import { Groups } from '@screens/Groups'
 import theme from './src/theme'
@@ -13,6 +14,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded
         ? <Groups />
         : <Loading />
