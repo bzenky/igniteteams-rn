446aec80c34df3b9e8d57128ea9d8f57b3a024ec
Zenky <bzenky@gmail.com>
2023-02-16 21:21:52 -0300

    feat: empty list component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
@@ -23,6 +24,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
           />
         )}
       />
