41a585cd4adad2d3138cdf5ac492256809fd248d
Zenky <bzenky@gmail.com>
2023-02-17 06:24:45 -0300

    feat: button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
 import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
@@ -32,6 +33,10 @@ export function Groups() {
             message='Que tal cadastrar a primeira turma?'
           />
         )}
+      />
+
+      <Button
+        title='Criar nova turma'
       />
     </Container>
   )
