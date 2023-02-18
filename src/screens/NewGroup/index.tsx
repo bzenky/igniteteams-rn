c68a2e69ebd256fa599015bd69c4a25e60f3009f
Zenky <bzenky@gmail.com>
2023-02-18 08:57:48 -0300

    feat: input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@components/Button"
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
+import { Input } from "@components/Input"
 import { Container, Content, Icon } from "./styles"
 
-export function NewGroup() {
+export function NewGroup({ }) {
   return (
     <Container>
       <Header showBackButton />
@@ -16,8 +17,13 @@ export function NewGroup() {
           subtitle='crie a turma para adicionar as pessoas'
         />
 
+        <Input
+          placeholder="Nome da turma"
+        />
+
         <Button
           title='Criar'
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
