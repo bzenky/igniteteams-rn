c194c47957ec666e52d1525e723acfbf00facde9
Zenky <bzenky@gmail.com>
2023-02-19 11:00:55 -0300

    feat: listing filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
-import { ButtonIcon } from "@components/ButtonIcon";
-import { Filter } from "@components/Filter";
-import { Header } from "@components/Header";
-import { Highlight } from "@components/Highlight";
-import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
+import { useState } from "react"
+import { FlatList } from "react-native"
+import { ButtonIcon } from "@components/ButtonIcon"
+import { Filter } from "@components/Filter"
+import { Header } from "@components/Header"
+import { Highlight } from "@components/Highlight"
+import { Input } from "@components/Input"
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles"
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,7 +30,25 @@ export function Players() {
         />
       </Form>
 
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
     </Container>
   )
 }
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native"
+import styled, { css } from "styled-components/native"
 
 export const Container = styled.View`
   flex: 1;
@@ -12,5 +12,19 @@ export const Form = styled.View`
   width: 100%;
   border-radius: 6px;
   background: ${({ theme }) => theme.COLORS.GRAY_700};
+`
 
+export const HeaderList = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+  width: 100%;
+`
+
+export const NumbersOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
 `
