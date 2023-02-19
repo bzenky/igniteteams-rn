dd3d553c87efe6f04fb3fea01ca61f9a14d296b7
Zenky <bzenky@gmail.com>
2023-02-19 15:07:03 -0300

    feat: stack navigator

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,27 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Groups } from "@screens/Groups"
+import { NewGroup } from "@screens/NewGroup"
+import { Players } from "@screens/Players"
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen
+        name="Groups"
+        component={Groups}
+      />
+
+      <Screen
+        name="NewGroup"
+        component={NewGroup}
+      />
+
+      <Screen
+        name="Players"
+        component={Players}
+      />
+    </Navigator>
+  )
+}
