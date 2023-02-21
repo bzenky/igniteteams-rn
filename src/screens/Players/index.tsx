4829b4ea06bfe082777bb70ff9b925c210024c77
Zenky <bzenky@gmail.com>
2023-02-21 20:57:46 -0300

    feat: players storage

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FlatList } from "react-native"
+import { Alert, FlatList } from "react-native"
 import { useRoute } from "@react-navigation/native"
 import { ButtonIcon } from "@components/ButtonIcon"
 import { Filter } from "@components/Filter"
@@ -10,17 +10,48 @@ import { PlayerCard } from "@components/PlayerCard"
 import { ListEmpty } from "@components/ListEmpty"
 import { Button } from "@components/Button"
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles"
+import { AppError } from "@utils/AppError"
+import { playerAddByGroup } from "@storage/player/playerAddByGroup"
+import { playersGetByGroup } from "@storage/player/playersGetByGroup"
 
 type RouteParams = {
   group: string
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState(['Zenky', 'Xablau'])
 
   const route = useRoute()
   const { group } = route.params as RouteParams
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova Pessoa', 'Informe o nome da pessoa para adicionar')
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group)
+
+      const players = await playersGetByGroup(group)
+      console.log(players)
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova Pessoa', error.message)
+      } else {
+        console.log(error)
+        Alert.alert('Nova Pessoa', 'Não foi possível adicionar a pessoa')
+      }
+    }
+
+    setNewPlayerName('')
+  }
 
   return (
     <Container>
@@ -33,11 +64,13 @@ export function Players() {
 
       <Form>
         <Input
+          onChangeText={setNewPlayerName}
           placeholder="Nome da pessoa"
           autoCorrect={false}
         />
         <ButtonIcon
           icon="add"
+          onPress={handleAddPlayer}
         />
       </Form>
 
---
+++ src/storage/player/playerAddByGroup.ts
@@ -0,0 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { PLAYER_COLLECTION } from "@storage/storageConfig"
+import { AppError } from "@utils/AppError"
+import { playersGetByGroup } from "./playersGetByGroup"
+import { PlayerStorageDTO } from "./PlayerStorageDTO"
+
+
+export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
+  try {
+    const storedPlayers = await playersGetByGroup(group)
+
+    const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name)
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError("Já existe um jogador cadastrado com esse nome.")
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer])
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage)
+
+  } catch (error) {
+    throw error
+  }
+}
