b1185fce9717d9e8de6fb4605079afba93ae2adc
Zenky <bzenky@gmail.com>
2023-02-21 06:29:28 -0300

    feat: implement create/get storage data

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,35 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
-import { Container } from './styles'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { groupsGetAll } from '@storage/group/groupsGetAll'
+import { Container } from './styles'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos'])
+  const [groups, setGroups] = useState<string[]>([])
 
   const navigation = useNavigation()
 
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroups()
+  }, []))
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,14 +4,20 @@ import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
 import { Input } from "@components/Input"
 import { useNavigation } from "@react-navigation/native"
+import { groupCreate } from "@storage/group/groupCreate"
 import { Container, Content, Icon } from "./styles"
 
 export function NewGroup({ }) {
   const [group, setGroup] = useState('')
   const navigation = useNavigation()
 
-  function handleNew() {
-    navigation.navigate('players', { group })
+  async function handleNew() {
+    try {
+      await groupCreate(group)
+      navigation.navigate('players', { group })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
