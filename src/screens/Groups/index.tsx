68dc71c701a2fc582c01103c44786a64d3a375fb
Zenky <bzenky@gmail.com>
2023-02-22 06:38:15 -0300

    feat: loading in fetchs

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,17 @@
 import { useCallback, useState } from 'react'
-import { FlatList } from 'react-native'
+import { Alert, FlatList } from 'react-native'
 import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { Loading } from '@components/Loading'
 import { groupsGetAll } from '@storage/group/groupsGetAll'
 import { Container } from './styles'
 
 export function Groups() {
+  const [isLoading, setIsLoading] = useState(true)
   const [groups, setGroups] = useState<string[]>([])
 
   const navigation = useNavigation()
@@ -20,10 +22,13 @@ export function Groups() {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true)
       const data = await groupsGetAll()
       setGroups(data)
+      setIsLoading(false)
     } catch (error) {
       console.log(error)
+      Alert.alert('Turmas', 'Não foi possível carregar as turmas')
     }
   }
 
@@ -46,22 +51,28 @@ export function Groups() {
         subtitle='jogue com sua turma'
       />
 
-      <FlatList
-        data={groups}
-        keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <GroupCard
-            title={item}
-            onPress={() => handleOpenGroup(item)}
+      {isLoading
+        ? <Loading />
+        : (
+          <FlatList
+            data={groups}
+            keyExtractor={item => item}
+            renderItem={({ item }) => (
+              <GroupCard
+                title={item}
+                onPress={() => handleOpenGroup(item)}
+              />
+            )}
+            contentContainerStyle={groups.length === 0 && { flex: 1 }}
+            ListEmptyComponent={() => (
+              <ListEmpty
+                message='Que tal cadastrar a primeira turma?'
+              />
+            )}
           />
-        )}
-        contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => (
-          <ListEmpty
-            message='Que tal cadastrar a primeira turma?'
-          />
-        )}
-      />
+        )
+      }
+
 
       <Button
         title='Criar nova turma'
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,19 +9,21 @@ import { Input } from "@components/Input"
 import { PlayerCard } from "@components/PlayerCard"
 import { ListEmpty } from "@components/ListEmpty"
 import { Button } from "@components/Button"
+import { Loading } from "@components/Loading"
 import { AppError } from "@utils/AppError"
 import { playerAddByGroup } from "@storage/player/playerAddByGroup"
 import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO"
 import { playersGetByGroupAndTeam } from "@storage/player/playersGetByGroupAndTeam"
 import { playerRemoveByGroup } from "@storage/player/playerRemoveByGroup"
-import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles"
 import { groupRemoveByName } from "@storage/group/groupRemoveByName"
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles"
 
 type RouteParams = {
   group: string
 }
 
 export function Players() {
+  const [isLoading, setIsLoading] = useState(true)
   const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
@@ -60,8 +62,10 @@ export function Players() {
 
   async function fetchPlayersByTeam() {
     try {
+      setIsLoading(true)
       const playersByTeam = await playersGetByGroupAndTeam(group, team)
       setPlayers(playersByTeam)
+      setIsLoading(false)
     } catch (error) {
       console.log(error)
       Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.')
@@ -153,26 +157,31 @@ export function Players() {
         </NumbersOfPlayers>
       </HeaderList>
 
-      <FlatList
-        data={players}
-        keyExtractor={item => item.name}
-        renderItem={({ item }) => (
-          <PlayerCard
-            name={item.name}
-            onRemove={() => handlePlayerRemove(item.name)}
+      {isLoading
+        ? <Loading />
+        : (
+          <FlatList
+            data={players}
+            keyExtractor={item => item.name}
+            renderItem={({ item }) => (
+              <PlayerCard
+                name={item.name}
+                onRemove={() => handlePlayerRemove(item.name)}
+              />
+            )}
+            ListEmptyComponent={() => (
+              <ListEmpty
+                message='Não há pessoas nesse time.'
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={[
+              { paddingBottom: 100 },
+              players.length === 0 && { flex: 1 }
+            ]}
           />
-        )}
-        ListEmptyComponent={() => (
-          <ListEmpty
-            message='Não há pessoas nesse time.'
-          />
-        )}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={[
-          { paddingBottom: 100 },
-          players.length === 0 && { flex: 1 }
-        ]}
-      />
+        )
+      }
 
       <Button
         title="Remover turma"
